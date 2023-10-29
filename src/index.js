633bb48c5a9b75b550f55992dcfa5d5d53bbbd79
Marta Mohedano <139046036+mmohedano@users.noreply.github.com>
2023-10-29 19:38:44 +0000

    tailwind template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import Example from './App';
 
 const root = createRoot(document.getElementById('app'));
 
 root.render(
   <StrictMode>
-    <App />
+    <Example />
   </StrictMode>
 );
